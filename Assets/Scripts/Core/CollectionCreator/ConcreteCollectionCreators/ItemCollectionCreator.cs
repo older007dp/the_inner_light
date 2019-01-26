1f03381e150d8507033e3d6cf16c4e0ea33a86c4
vadim_vengerovsky <vadim.vengerovsky@dev-pro.net>
2019-01-26 14:53:25 +0200

    added collection creator for item views

---
+++ Assets/Scripts/Core/CollectionCreator/ConcreteCollectionCreators/ItemCollectionCreator.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using DataStructures;
+
+namespace Core.CollectionCreator.ConcreteCollectionCreators
+{
+    public class ItemCollectionCreator : CollectionCreator<ItemData, ItemView>
+    {
+        
+    }
+}
