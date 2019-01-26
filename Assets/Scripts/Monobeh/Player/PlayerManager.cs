b9e86ac60c9e182ff78abd32a9aa7e15075c5444
Oleh <oleh.tolstoi@dev-pro.net>
2019-01-26 12:29:15 +0200

    physics settup, character movement

---
+++ Assets/Scripts/Monobeh/Player/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Monobeh.Player
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        private Rigidbody2D CharacterController;
+        private void Start()
+        {
+            CharacterController = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Movement();
+        }
+    
+        private void Movement()
+        {
+            var xAxis = Input.GetAxis("Horizontal");
+            var yAxis = Input.GetAxis("Vertical");
+        
+            var moveVector = new Vector2(xAxis, yAxis);
+
+            Debug.Log(moveVector);
+
+            CharacterController.velocity = moveVector;
+        }
+    }
+}
