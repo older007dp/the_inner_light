c213ca0be8831f2233958d13a6c1e23028d0adad
Oleh <oleh.tolstoi@dev-pro.net>
2019-01-28 09:16:35 +0200

    added button

---
+++ Assets/ApplicationExit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApplicationExit : MonoBehaviour
+{
+    [SerializeField] private Button ExitButton;
+    private void Awake()
+    {
+        ExitButton.onClick.AddListener(ApplicationQuit);
+    }
+
+    private void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
